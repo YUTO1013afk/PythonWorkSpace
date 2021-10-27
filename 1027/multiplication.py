dc5a2d837777907cd206308917f9f9099f7bb863
Yuto Tajima <yourentiandao304@gmail.com>
2021-10-27 11:35:23 +0900

    multiplication.py Yuto Tajima

---
+++ 1027/multiplication.py
@@ -0,0 +1,7 @@
+range = [1,2,3,4,5,6,7,8,9]
+multiplication = [1,2,3,4,5,6,7,8,9]
+for i in range[1:2]:
+    multiplication.append(i)
+    for j in range[1:10]:
+        multiplication.append(i*j) 
+print(multiplication)
