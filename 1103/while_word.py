19682510de9b854b66e95b60bba10b0d165ffda0
Yuto Tajima <yourentiandao304@gmail.com>
2021-11-03 22:27:16 +0900

    while_word.py Yuto Tajima

--- 1103/while_word.py
+++ 1103/while_word.py
@@ -1,4 +1,11 @@
+import os
 list = []
+if os.path.exists("word.txt") == True:
+    f = open('word.txt', 'r')
+    list = f.readlines()
+    for i, v in enumerate(list):
+        list[i] = v.replace("\n", "")
+    f.close()    
 while True:
     n = input("単語を入力してください：")
     if n == "":
@@ -12,5 +19,9 @@ while True:
         continue
     if n != "LIST":
         list.append(n)
+        file = open('word.txt', 'w')
+        for d in list:
+            file.write("%s\n" % d)
+        file.close()
 print("これまでに覚えた単語：",end="")
 print(list)
