9a34dc093f80b5e329020921ad9a2f64c1f93953
Yuto Tajima <yourentiandao304@gmail.com>
2021-11-03 23:35:00 +0900

    linenum.py Yuto Tajima

--- 1103/linenum.py
+++ 1103/linenum.py
@@ -6,7 +6,6 @@ while True:
     line = line.replace("\n", "")
     if line:
         row_no += 1
-        print("000",end="")
         row_no = str(row_no)
-        print(row_no + ":" + line)
+        print("000" + row_no + ":" + line)
         row_no = int(row_no)
