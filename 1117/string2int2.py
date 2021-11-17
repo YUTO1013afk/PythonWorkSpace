e732794c4dfe56a76d16d9ce9be73519d32f9852
Yuto Tajima <yourentiandao304@gmail.com>
2021-11-17 12:30:25 +0900

    string2int2.py Yuto Tajima

---
+++ 1117/string2int2.py
@@ -0,0 +1,21 @@
+def list2int(s):
+    """list2int 文字列を数値に変換した値を返す（List 対応）"""
+    if(type(s) == str):
+        if(s.isalpha() == True):
+            return 0
+    if(type(s) == str):
+        if(s.isdigit() == True):
+            return int(s)
+    if(type(s) == list):
+        for i in range(len(s)):
+            if(type(s[i]) == str):
+                if(s[i].isdigit() == True):
+                    s[i] = int(s[i])
+                elif(s[i].isalpha() == True):
+                    s[i] = 0
+        return s
+    else:
+        return s
+print(list2int(['5','ab','100',10,1]))
+print(list2int('100'))
+print(list2int('xyz'))
