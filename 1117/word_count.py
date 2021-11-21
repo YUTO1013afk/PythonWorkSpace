a7f3f426b1203d899ace3a1c71e9e1dac8d4699a
Yuto Tajima <yourentiandao304@gmail.com>
2021-11-22 00:12:49 +0900

    word_count.py Yuto Tajima

--- 1117/word_count.py
+++ 1117/word_count.py
@@ -5,9 +5,11 @@ for i in f:
     word.append(i.rstrip('\n'))
 f.close
 word_count = {}
+word = sorted(word)
 for j in word:
     if j in word_count:
         word_count[j] += 1
     else:
         word_count[j] = 1
-print(word_count)
+for i in word_count:
+    print('%s :%d' % (i, word_count[i]))
