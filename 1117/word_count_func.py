38734dd8d7a5d24448843dda1a48015a3380e714
Yuto Tajima <yourentiandao304@gmail.com>
2021-11-23 15:27:42 +0900

    word_count_func.py Yuto Tajima

---
+++ 1117/word_count_func.py
@@ -0,0 +1,20 @@
+import re
+f = open('sentence.txt')
+data = f.read()
+f.close
+
+# counting
+words = {}
+data = re.split(r'\s+|\,|\.|\(|\)|\"|\:', data.lower())
+for item in data:
+    if len(item)-1 < 1:  
+        data.remove("")
+data.sort()
+for word in data:
+    words[word] = words.get(word, 0) + 1
+    word.strip()
+
+# sort by count
+d = [(v,k) for k,v in words.items()]
+for count, word in d[:]:
+    print ('%s :%d' % (word, count))
