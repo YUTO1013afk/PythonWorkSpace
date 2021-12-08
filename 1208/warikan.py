da337ec3d26692e7607f317f512d61bf1cc845b7
Yuto Tajima <yourentiandao304@gmail.com>
2021-12-09 01:38:33 +0900

    warikan.py Yuto Tajima

--- 1208/warikan.py
+++ 1208/warikan.py
@@ -5,7 +5,7 @@ def calc_payment(sum,num):
     pay = math.ceil((sum / num) / 100) * 100 
     balance = sum - pay * (num-1)
     return [pay,balance]
-def output_paymen(payment,num):
+def output_payment(payment,num):
     num -= 1
     print(f'支払金額：' + str("{:,d}".format(payment[0])) + "円" + " (" + str(num) + "人)")
     print(f'幹事金額：' + str("{:,d}".format(payment[1])) + "円")
@@ -13,4 +13,4 @@ def output_paymen(payment,num):
 sum = input_int(input("支払合計金額を入力してください："))
 num = input_int(input("参加者人数を入力してください："))
 payment = calc_payment(sum,num)
-output_paymen(payment,num)
+output_payment(payment,num)
