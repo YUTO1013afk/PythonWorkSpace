07dcac58f6fc08c2fe305fb8b5d650f20b14903b
Yuto Tajima <yourentiandao304@gmail.com>
2021-12-09 10:33:27 +0900

    lambda_3.py Yuto Tajima

--- 1208/lambda_3.py
+++ 1208/lambda_3.py
@@ -1,4 +1,4 @@
 my_list = ['hello','nice','abc','test','morning','good','yes','world']
-new_list =  list(filter(lambda n : len(n)>=5, my_list))
+new_list = list(filter(lambda n : len(n)>=5, my_list))
 print(my_list)
 print(new_list)
