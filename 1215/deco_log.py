81b529868213c6a2d4bfa9d6008fe724af309d37
Yuto Tajima <yourentiandao304@gmail.com>
2021-12-17 03:07:44 +0900

    deco_log.py Yuto Tajima

---
+++ 1215/deco_log.py
@@ -0,0 +1,24 @@
+import datetime
+
+def log_file(func):
+    def function(*args,**kwargs):
+        with open('python.log.txt', 'a') as f:
+            reslut = func(*args,**kwargs)
+            f.write(str(datetime.datetime.now()) + " ")
+            f.write('function:' + func.__name__ + " ")
+            f.write("args:" + str(args) + " ")
+            f.write("kwargs:" + str(kwargs) + "\n")
+            return reslut
+    return function
+
+# main
+@log_file
+def test(n):
+    print('test->{}'.format(n))
+
+# 呼出
+test(100)
+
+# 呼出
+for i in range(5):
+    test(i)
