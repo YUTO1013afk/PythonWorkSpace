eb51feaca99224fab9709da5cf058f7d959a27cc
Yuto Tajima <yourentiandao304@gmail.com>
2021-12-18 23:48:11 +0900

    list_calc2.py Yuto Tajima

---
+++ list_calc2.py
@@ -0,0 +1,35 @@
+sym = ["+","-","*","/"]
+result = []
+while True:
+    calc = input("calc :")
+    if (calc == "="):
+        break
+    elif (calc.isdigit() == False):
+        if (calc not in sym):
+            print("入力が正しくありません。")
+            continue
+    elif (len(result) > 0):
+        if (str(result[-1]).isdigit() == True and calc.isdigit() == True):
+            print("入力が正しくありません。")
+            continue 
+    result.append(calc)
+    if (result[0] in sym):
+        print("入力が正しくありません。")
+        result.clear()
+        continue
+    elif (len(result) > 1):
+        if (result[-2] in sym and calc in sym):
+            if (result[-2] != "*" or result[-2] == "*" and calc != "*"):
+                print("入力が正しくありません。")
+                del result[-1]
+                continue
+        if (result[-2] == "/" and calc == "0"):
+            print("入力が正しくありません。")
+            del result[-1]
+            continue
+print("入力した計算式：", end="")
+for i in range(len(result)):
+    print(result[i], end="")
+    sum = (''.join(result))
+print()
+print("計算結果：" + str(int(eval(sum))))
